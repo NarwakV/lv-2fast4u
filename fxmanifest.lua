0a27696bdb5c3baea1628d5e108d634c8eb4e587
NarwakV <84711305+NarwakV@users.noreply.github.com>
2022-03-22 13:49:06 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,12 +5,9 @@ description 'QB-2FAST4U'
 author 'Narwak'
 version '1.0.0'
 
-shared_scripts { 
-	'@qb-core/import.lua',
-}
 
 client_script 'client.lua'
-client_script 'tuner.lua'
+--client_script 'tuner.lua'
 
 server_script 'server.lua'
 
